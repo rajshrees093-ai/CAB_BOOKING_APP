caa2d1ad825ccf7f6e8bbed760c0b01a083f8433
Rajshree <rajshrees093@gmail.com>
2026-03-08 00:19:28 +0530

    day 7

--- cab_booking_app/tests/rides.test.js
+++ cab_booking_app/tests/rides.test.js
@@ -2,12 +2,12 @@ const request = require("supertest");
 
 describe("Ride API Test", () => {
 
-  test("Fetch latest ride", async () => {
+  test("Unauthorized request should return 401", async () => {
 
     const res = await request("http://localhost:3000")
       .get("/api/rides/latest");
 
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(401);
 
   });
 
