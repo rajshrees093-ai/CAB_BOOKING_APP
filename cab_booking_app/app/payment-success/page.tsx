e5450c255530622f4be79acee1449c7d47321e0b
Rajshree <rajshrees093@gmail.com>
2026-03-09 23:28:30 +0530

    day 8

---
+++ cab_booking_app/app/payment-success/page.tsx
@@ -0,0 +1,17 @@
+export default function PaymentSuccess(){
+
+  return(
+
+    <div className="min-h-screen flex items-center justify-center">
+
+      <div className="bg-green-500 text-white p-6 rounded">
+
+        Payment Successful 🚕
+
+      </div>
+
+    </div>
+
+  )
+
+}
