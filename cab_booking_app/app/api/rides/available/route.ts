df769a0183c62946c4d830db4529321a859d1ed6
Rajshree <rajshrees093@gmail.com>
2026-03-16 23:40:30 +0530

    day 9

--- cab_booking_app/app/api/rides/available/route.ts
+++ cab_booking_app/app/api/rides/available/route.ts
@@ -2,17 +2,29 @@ import { supabase } from "@/lib/supabase";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  try {
 
-  const { data: rides, error } = await supabase
-    .from("rides")
-    .select("*")
-    .eq("status", "requested")
-    .order("created_at", { ascending: false });
+    const { data: rides, error } = await supabase
+      .from("rides")
+      .select("*")
+      .eq("status", "requested")
+      .order("created_at", { ascending: false });
 
-  if (error) {
-    return NextResponse.json({ error: error.message });
+    if (error) {
+      console.log(error);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json({ rides });
+
+  } catch (error) {
+
+    console.log(error);
+
+    return NextResponse.json(
+      { error: "Server error" },
+      { status: 500 }
+    );
+
   }
-
-  return NextResponse.json({ rides });
-
 }
